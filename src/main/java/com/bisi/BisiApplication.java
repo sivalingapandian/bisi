d288f7f0e173e2a52d34d9c6c63b765d276b2e47
pandian <Pandian.Velayutham@medhost.com>
2016-11-05 12:39:54 -0500

    initial commit

---
+++ src/main/java/com/bisi/BisiApplication.java
@@ -0,0 +1,12 @@
+package com.bisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisiApplication.class, args);
+	}
+}
